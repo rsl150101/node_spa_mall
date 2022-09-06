13a563df76189b62475d87e7973f0d2b5b0a4879
ArchaeG <archepro84@gmail.com>
2022-09-07 02:28:31 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+const connect = require("./schemas");
+const goodsRouter = require("./routes/goods");
+const cartsRouter = require("./routes/carts");
+
+connect();
+
+app.use(express.json());
+
+app.use("/api", [goodsRouter, cartsRouter]);
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(port, "포트로 서버가 열렸어요!");
+});
