6df123e3314ca274e899947747c2ce3f2efa22c0
rsl150101 <dnwnd501@naver.com>
2022-12-25 22:58:42 +0900

    입문 과정에 숙련 과정 합치기: 서버 구축, 프론트 추가, JWT 추가 숙련 과정에 로그인, 회원가입 기능 추가를 위해 JWT 추가, 기존의 서버 구축 주석 처리하고 숙련 과정 서버로 코드 실행, 유저 모델 추가

--- app.js
+++ app.js
@@ -1,21 +1,41 @@
+// const express = require("express");
+// const app = express();
+// const port = 3000;
+
+// const connect = require("./schemas");
+// const goodsRouter = require("./routes/goods");
+// const cartsRouter = require("./routes/carts");
+
+// connect();
+
+// app.use(express.json());
+
+// app.use("/api", [goodsRouter, cartsRouter]);
+
+// app.get("/", (req, res) => {
+//   res.send("Hello World!");
+// });
+
+// app.listen(port, () => {
+//   console.log(port, "포트로 서버가 열렸어요!");
+// });
+
 const express = require("express");
-const app = express();
-const port = 3000;
+const mongoose = require("mongoose");
 
-const connect = require("./schemas");
-const goodsRouter = require("./routes/goods");
-const cartsRouter = require("./routes/carts");
-
-connect();
-
-app.use(express.json());
-
-app.use("/api", [goodsRouter, cartsRouter]);
-
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+mongoose.connect("mongodb://localhost:27017/shopping-demo", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
 
-app.listen(port, () => {
-  console.log(port, "포트로 서버가 열렸어요!");
+const app = express();
+const router = express.Router();
+
+app.use("/api", express.urlencoded({ extended: false }), router);
+app.use(express.static("assets"));
+
+app.listen(8080, () => {
+  console.log("서버가 요청을 받을 준비가 됐어요");
 });
