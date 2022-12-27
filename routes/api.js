93b99ece550e5d611f357e83321e2de14b6f4b21
rsl150101 <dnwnd501@naver.com>
2022-12-28 01:49:00 +0900

    저장 안해서 저장 후 다시 커밋

--- routes/api.js
+++ routes/api.js
@@ -69,11 +69,34 @@ router.post("/users", async (req, res) => {
   res.status(201).send({});
 });
 
-//* 로그인 API
+//* 로그인 API - mongodb
+// router.post("/auth", async (req, res) => {
+//   const { email, password } = req.body;
+
+//   const user = await User.findOne({ email });
+
+//   // NOTE: 인증 메세지는 자세히 설명하지 않는것을 원칙으로 한다: https://cheatsheetseries.owasp.org/cheatsheets/Authentication_Cheat_Sheet.html#authentication-responses
+//   if (!user || password !== user.password) {
+//     res.status(400).send({
+//       errorMessage: "이메일 또는 패스워드가 틀렸습니다.",
+//     });
+//     return;
+//   }
+
+//   res.send({
+//     token: jwt.sign({ userId: user.userId }, "customized-secret-key"),
+//   });
+// });
+
+//* 로그인 API - MySQL
 router.post("/auth", async (req, res) => {
   const { email, password } = req.body;
 
-  const user = await User.findOne({ email });
+  const user = await User.findOne({
+    where: {
+      email,
+    },
+  });
 
   // NOTE: 인증 메세지는 자세히 설명하지 않는것을 원칙으로 한다: https://cheatsheetseries.owasp.org/cheatsheets/Authentication_Cheat_Sheet.html#authentication-responses
   if (!user || password !== user.password) {
