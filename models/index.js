f93401c6e29259dca2c2739e52d07da4fe1e2496
rsl150101 <dnwnd501@naver.com>
2022-12-28 01:16:41 +0900

    MongoDB -> MySQL

--- models/index.js
+++ models/index.js
@@ -1,13 +1,38 @@
-const mongoose = require("mongoose");
+'use strict';
 
-const connect = () => {
-  mongoose
-    .connect("mongodb://localhost:27017/spa_mall")
-    .catch(err => console.log(err));
-};
+const fs = require('fs');
+const path = require('path');
+const Sequelize = require('sequelize');
+const process = require('process');
+const basename = path.basename(__filename);
+const env = process.env.NODE_ENV || 'development';
+const config = require(__dirname + '/../config/config.json')[env];
+const db = {};
 
-mongoose.connection.on("error", err => {
-  console.error("몽고디비 연결 에러", err);
+let sequelize;
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+} else {
+  sequelize = new Sequelize(config.database, config.username, config.password, config);
+}
+
+fs
+  .readdirSync(__dirname)
+  .filter(file => {
+    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
+  })
+  .forEach(file => {
+    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
+    db[model.name] = model;
+  });
+
+Object.keys(db).forEach(modelName => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
 });
 
-module.exports = connect;
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+module.exports = db;
